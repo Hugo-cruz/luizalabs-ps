cac07cae4944fe06b145da1767dc0540b5db32b8
Hugo-cruz <hugo.fellipe.cruz@gmail.com>
2021-07-24 14:53:35 -0300

    added dockerfile

--- app/__main__.py
+++ app/__main__.py
@@ -4,12 +4,6 @@
 
 import os
 
-import newrelic.agent
-
-# Initialize newrelic agent before importing anything else
-# The newrelic config file is hardcoded because we can't import nothing before this :-(
-newrelic.agent.initialize("{}/newrelic.ini".format(os.getcwd()))
-
 from . import create_app
 
 app = create_app()
